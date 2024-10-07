91b0c3a4c03732bae61a6dc15820a10b484423b0
carlotamartins <carlota.martins2003@gmail.com>
2024-10-07 21:02:50 +0100

    updated

---
+++ Assignment 2/data_filtering.py
@@ -0,0 +1,7 @@
+#Creating a mixed list
+characteristics = ["Carlota", 21, 1.68, "Maribel", 63, 1.65, "Diogo", 21, 1.54]
+
+
+#Creating a new list where only the integers are present and printing it
+filtered_list = [item for item in characteristics if isinstance(item, int)]
+print(filtered_list)
